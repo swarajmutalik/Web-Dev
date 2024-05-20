6607695422f06aa882589d73acb97e6e8528300b
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-05-20 21:29:12 +0530

    Day 35

--- Day-35/script.js
+++ Day-35/script.js
@@ -1,3 +1,20 @@
 alert("Hello World");
 
-console.log("Hello");
+console.log("Code is running...");
+console.log("Code is  also running...");
+console.log("Code is looking like a wow...");
+
+var a = prompt("Enter you number");
+var isTrue = confirm(
+  "Are you sure you want to leave this page and blast your computer "
+);
+
+if (isTrue) {
+  console.log("Complier is blasting");
+} else {
+  console.log("Computer is not blasting");
+}
+
+console.log("Your number is " + a)
+
+document.title = "Hey I am good"
