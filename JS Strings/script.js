2bb200968f1fde3fc4ff9615b2bb74cd97501a0b
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2025-02-25 12:36:38 +0530

    Create script.js

---
+++ JS Strings/script.js
@@ -0,0 +1,18 @@
+let a = "Swaraj";
+console.log(a);
+console.log(a[0]);
+console.log(a.length);
+
+
+let real_name = "Swaraj";
+let friend = "Arun";
+console.log(real_name + " and " + friend);
+
+let b = "Swarika";
+console.log(b.toLocaleUpperCase());
+console.log(b.toLocaleLowerCase());
+console.log(b.slice(1,5));
+console.log(b.slice(1));
+
+console.log(b.replace("Swa","Ra"));
+console.log(b.concat(b," is a good person"))
