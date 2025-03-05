4f293be228f805a10a3abffbbf01a2330c7f3cd8
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2025-03-05 11:45:11 +0530

    Create script.js

---
+++ JS Callbacks and Promises/script.js
@@ -0,0 +1,33 @@
+console.log("He is a Hacker");
+console.log("He is a Hecker");
+
+setTimeout(() => {
+  console.log("I am inside settimeout");
+}, 0);
+
+setTimeout(() => {
+  console.log("I am inside settimeout-2");
+}, 0);
+
+console.log("The End");
+
+const fn = () => {
+  console.log("Nothing");
+};
+
+const callback = (arg, fn) => {
+  console.log(arg);
+  fn();
+};
+
+const loadScript = (src, callback) => {
+  let sc = document.createElement("script");
+  sc.src = src;
+  sc.onload = callback("Swaraj", fn);
+  document.head.append(sc);
+};
+
+loadScript(
+  "https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js",
+  callback
+);
