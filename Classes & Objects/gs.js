d943a33632e0aac8ce3c2a25ed088364c0145f41
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2025-03-07 17:36:00 +0530

    Create gs.js

---
+++ Classes & Objects/gs.js
@@ -0,0 +1,23 @@
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (value.length < 4) {
+      console.log("Name is too short.");
+      return;
+    }
+    this._name = value;
+  }
+}
+
+let user = new User("John");
+console.log(user.name);
+
+user.name = "Swaraj";
+console.log(user.name);
