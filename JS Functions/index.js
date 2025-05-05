329896bc635ca06d6ddfaaabe7db7d895e0081fc
swarajmutalik <swarajmutalik@gmail.com>
2025-05-05 12:23:52 +0530

    Functions

--- JS Functions/index.js
+++ JS Functions/index.js
@@ -1,29 +1,19 @@
 function nice(name) {
-    console.log("Hey " + name + " you are nice!")
-    console.log("Hey " + name + " you are good!")
-    console.log("Hey " + name + " your tshirt is nice!")
-    console.log("Hey " + name + " your course is good too!")
+    console.log("Hey " + name + " you are nice.");
+    console.log("Hey " + name + " you are good.");
+    console.log("Hey " + name + " your tshirt is looking good.");
+}
+
+const func1 = (x) => {
+    console.log("I am an arrow function", x); 
 }
 
 function sum(a, b, c = 3) {
-    // console.log(a + b)
-    return a + b + c
+    return a + b + c;
 }
 
+result = sum(4, 5);
+console.log("The result of these numbers is " + result);
 
-result1 = sum(3, 2)
-result2 = sum(7, 5)
-result3 = sum(3, 13, 1)
-
-console.log("The sum of these numbers is: ", result1)
-console.log("The sum of these numbers is: ", result2)
-console.log("The sum of these numbers is: ", result3)
-
-
-const func1 = (x)=>{
-    console.log("I am an arrow function", x)
-}
-
+nice('Swaraj');
 func1(34);
-func1(66);
-func1(84);
